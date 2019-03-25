ef4971fac7e32e0d948fc8e2ff8101cedc55245a
1443213244 <1443213244@qq.com>
2019-03-25 19:08:20 +0800

    Create getRand.sql

---
+++ getRand.sql
@@ -0,0 +1,23 @@
+CREATE DEFINER = CURRENT_USER FUNCTION `getRand` ( `counts` INTEGER ) RETURNS VARCHAR ( 20 ) BEGIN
+DECLARE
+	sTemp VARCHAR ( 20 );
+DECLARE
+	sTempCounts INTEGER;
+
+SET sTemp = ROUND( ROUND( RAND( ), counts ) * ( POW( 10, counts ) ) );
+IF
+	( CHAR_LENGTH( sTemp ) < counts ) THEN
+		
+		SET sTempCounts = counts - CHAR_LENGTH( sTemp );
+	
+	SET sTemp = CONCAT( sTemp, RIGHT ( CONCAT( POW( 10, sTempCounts ), '' ), sTempCounts ) );
+	
+END IF;
+IF
+	( CHAR_LENGTH( sTemp ) > counts ) THEN
+		
+		SET sTemp = RIGHT ( sTemp, counts );
+	
+END IF;
+RETURN sTemp;
+END;
