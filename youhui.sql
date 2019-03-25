9d9cffdcac64945bbd9110c3d2bd1b607539184f
1443213244 <1443213244@qq.com>
2019-03-25 19:09:24 +0800

    Create youhui.sql

---
+++ youhui.sql
@@ -0,0 +1,15 @@
+delimiter $$
+CREATE DEFINER=CURRENT_USER PROCEDURE `youhui`(IN num int, IN rmb int, IN tag varchar(50))
+BEGIN
+declare i int default 1;
+while i<=num do
+INSERT INTO `tblpromotions`
+( `code`, `type`, `recurring`,`value`, `cycles`, `appliesto`, `requires`,  `startdate`, 
+
+`expirationdate`, `maxuses`,`newsignups`)
+VALUES 
+(concat(tag,getrand(8)),'Fixed Amount',0,rmb,'','6','6',now(), DATE_ADD(now(), INTERVAL 1 YEAR),1,0);
+set i=i+1;
+end while;
+END $$
+delimiter ;
