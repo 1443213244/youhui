b72ba2e748cd36cd658c3bf961f4877ecd7b8e62
1443213244 <1443213244@qq.com>
2019-04-14 17:50:35 +0800

    Update youhui.sql

--- youhui.sql
+++ youhui.sql
@@ -1,5 +1,5 @@
 delimiter $$
-CREATE DEFINER=CURRENT_USER PROCEDURE `youhui`(IN num int, IN rmb int, IN tag varchar(50))
+CREATE DEFINER=CURRENT_USER PROCEDURE `youhui`(IN num int, IN rmb int, IN product int, IN tag varchar(50))
 BEGIN
 declare i int default 1;
 while i<=num do
@@ -8,7 +8,7 @@ INSERT INTO `tblpromotions`
 
 `expirationdate`, `maxuses`,`newsignups`)
 VALUES 
-(concat(tag,getrand(8)),'Fixed Amount',0,rmb,'','6','6',now(), DATE_ADD(now(), INTERVAL 1 YEAR),1,0);
+(concat(tag,getrand(8)),'Fixed Amount',0,rmb,'', product, product, now(), DATE_ADD(now(), INTERVAL 1 YEAR),1,0);
 set i=i+1;
 end while;
 END $$
